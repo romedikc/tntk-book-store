47c51c5fa32883ef8991455ee9b53271af7c7e38
Aiana <1ayanabdrahmanova1@gmail.com>
2024-06-29 20:19:51 +0600

    Include order routes

--- services/main.py
+++ services/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from services.auth import routes as AuthRoutes
 from services.books import routes as TaskRoutes
+from services.order import routes as OrderRoutes
 
 app = FastAPI()
 
 app.include_router(AuthRoutes.router)
 app.include_router(TaskRoutes.router)
+app.include_router(OrderRoutes.router)
