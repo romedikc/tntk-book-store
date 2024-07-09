3db293d5de65b8871e4c2d5ac79da907605987c4
Aiana <1ayanabdrahmanova1@gmail.com>
2024-07-09 20:46:56 +0600

    Add cors

--- services/main.py
+++ services/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from services.auth import routes as AuthRoutes
 from services.books import routes as TaskRoutes
@@ -9,3 +10,15 @@ app = FastAPI()
 app.include_router(AuthRoutes.router)
 app.include_router(TaskRoutes.router)
 app.include_router(OrderRoutes.router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
